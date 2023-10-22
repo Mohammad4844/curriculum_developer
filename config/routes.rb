e1c29d4dca0ccaecfa9ad4fea0095194f986505d
Mohammad <mohammadimaan484@gmail.com>
2023-10-22 06:11:21 -0400

    built a basic curriculum builder form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  post 'user_curriculum/develop', to: 'user_curriculum#develop', as: 'custom_route'
   devise_for :users
-  get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "welcome#index"
 end
